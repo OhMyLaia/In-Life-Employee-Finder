1d9aab9651b6b783b621feec03a7a8ddd306b5ac
OhMyLaia <ohmylaia@live.com>
2025-05-10 14:35:03 +0200

    refactor

--- src/views/ManagerView.vue
+++ src/views/ManagerView.vue
@@ -1,102 +1,79 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
-// store de tech para añadir al array
-import { useStackListStore } from '../stores/useStackListStore';
-// store de data.json
-import useEmployeeStore from '../stores/useEmployeeListStore';
-import type { EmployeeType } from '../types';
-import GenericButton from '../components/GenericButton.vue';
-import GenericInfoSpan from '../components/GenericInfoSpan.vue';
+    import { ref, onMounted } from 'vue';
+    import { useStackListStore } from '../stores/useStackListStore';
+    import useEmployeeStore from '../stores/useEmployeeListStore';
+    import type { EmployeeType } from '../types';
+    import GenericButton from '../components/GenericButton.vue';
 
-const stackListStore = useStackListStore();
-const employeeStore = useEmployeeStore();
+    const stackListStore = useStackListStore();
+    const employeeStore = useEmployeeStore();
 
-const name = ref('');
-// const id = ref('');
-const role = ref('');
-const location = ref('');
-const newTechnology = ref('');
-const description = ref('');
-const idToDelete = ref('');
-const isSubmittedAdd = ref<true | false | null>(null);
-const isSubmittedDelete = ref<true | false | null>(null);
+    const name = ref('');
+    const role = ref('');
+    const location = ref('');
+    const newTechnology = ref('');
+    const description = ref('');
+    const idToDelete = ref('');
 
-onMounted(() => {
-    employeeStore.fetchEmployees();
-});
+    onMounted(() => {
+        employeeStore.fetchEmployees();
+    });
 
-function addStackTechnology(newTech: string) {
-    stackListStore.addTech(newTech);
-    newTechnology.value = '';
-}
-
-async function handleSubmit() {
-
-    const employeeData: EmployeeType = {
-        name: name.value,
-        id: String(Date.now()),
-        role: role.value,
-        location: location.value,
-        stack: stackListStore.stack,
-        description: description.value,
-        picture: '/02.svg'
-    };
-
-    if (!name.value || !role.value || !location.value || !description.value) {
-        console.log(`error, missing some required data`);
-        isSubmittedAdd.value = false;
-        return
+    function addStackTechnology(newTech: string) {
+        stackListStore.addTech(newTech);
+        newTechnology.value = '';
     }
 
-    try {
-        await employeeStore.addEmployee(employeeData);
-        const success = employeeStore.addEmployee(employeeData);
-        isSubmittedAdd.value = success;
+    async function handleSubmit() {
+        const employeeData: EmployeeType = {
+            name: name.value,
+            id: String(Date.now()),
+            role: role.value,
+            location: location.value,
+            stack: stackListStore.stack,
+            description: description.value,
+            picture: '/02.svg'
+        };
 
-    } catch (error) {
-        isSubmittedAdd.value = false;
-        console.error("Error in handleSubmit:", error);
-    }
-    
-    name.value = '';
-    role.value = '';
-    location.value = '';
-    description.value = '';
-    newTechnology.value = '';
-    stackListStore.clearStack();
+        if (!name.value || !role.value || !location.value || !description.value) {
+            console.log(`error, missing some required data`);
+            return;
+        }
 
-    setTimeout(() => {
-        isSubmittedAdd.value = null;
-    }, 3000);
-}
+        try {
+            await employeeStore.addEmployee(employeeData);
+            alert(`Employee ${name.value} added successfully!`);
 
-
-async function handleDelete() {
-
-    if (!idToDelete.value) {
-        console.log('Please provide a valid ID');
-        isSubmittedDelete.value = false;
-        return;
-    }
-
-    const confirmed = window.confirm(`Are you sure you want to delete employee ID ${idToDelete.value}?`);
-    if (!confirmed) {
-        return;
+        } catch (error) {
+            console.error("Error in handleSubmit:", error);
+        }
+        
+        name.value = '';
+        role.value = '';
+        location.value = '';
+        description.value = '';
+        newTechnology.value = '';
+        stackListStore.clearStack();
     }
 
-    try {
-        await employeeStore.deleteEmployee(idToDelete.value);
-        isSubmittedDelete.value = true;
-        idToDelete.value = '';
-    } catch (error) {
-        isSubmittedDelete.value = false;
-        console.log('Failed to delete:', error);
-    }
 
-    setTimeout(() => {
-        isSubmittedDelete.value = null;
-    }, 3000);
-}
+    async function handleDelete() {
+
+        if (!idToDelete.value) {
+            console.log('Please provide a valid ID');
+            return;
+        }
+
+        const confirmed = window.confirm(`Are you sure you want to delete employee ID ${idToDelete.value}?`);
+        if (!confirmed) return;
+        try {
+            await employeeStore.deleteEmployee(idToDelete.value);
+            idToDelete.value = '';
+        } catch (error) {
+
+            console.log('Failed to delete:', error);
+        }
+    }
 </script>
 
 <template>
@@ -124,7 +101,6 @@ async function handleDelete() {
                     <label for="location" class="label"></label>
                     <input id="location" class="input" type="text" placeholder="Location" v-model="location">
                 </div>
-
                 <div class="has-text-left">
                     <div class="field text-is-small">
                         <label for="stack" class="label"></label>
@@ -147,7 +123,6 @@ async function handleDelete() {
                     <GenericButton type="button" :text="'Add tech'" :class="'button is-info is-light mb-5'"
                         @click="() => addStackTechnology(newTechnology)" />
                 </div>
-
                 <div class="field is-required">
                     <label for="textfield" class="label"></label>
                     <textarea id="textfield" class="textarea" placeholder="Description"
@@ -155,18 +130,7 @@ async function handleDelete() {
                 </div>
                 <GenericButton type="submit" :text="'Submit'" :class="'button is-info is-light'" />
             </form>
-            
         </div>
-        <div>
-            <p v-if="isSubmittedAdd === true">
-                <GenericInfoSpan :text="'Employee created successfully!'" customClass="has-text-info mt-2 mb-6" />
-            </p>
-    
-            <p v-if="isSubmittedAdd === false">
-                <GenericInfoSpan :text="'Something went wrong, try again.'" customClass="has-text-info mt-2 mb-6" />
-            </p>
-        </div>
-
         <h3 class="
             is-size-3-mobile
             is-size-3-tablet
@@ -177,28 +141,14 @@ async function handleDelete() {
             mt-6">
             Delete employee</h3>
 
-            <p v-if="isSubmittedDelete === true">
-            <GenericInfoSpan
-            :text="'Employee deleted from the database.'" 
-            customClass="has-text-info mb-2"
-            />
-        </p>
-        <p v-if="isSubmittedDelete === false">
-            <GenericInfoSpan
-            :text="'Error, could not delete employee. Please, try again.'"
-            customClass="has-text-info mb-2" />
-        </p>
-
         <div class="field is-required">
             <label for="delete-name" class="label">ID</label>
             <input id="delete-id" class="input" type="text" placeholder="Enter employee id to delete"
                 v-model="idToDelete" />
         </div>
-
         <GenericButton type="button"
         :text="'Delete'"
         :class="'button is-danger mb-3'"
         @click="handleDelete" />
-
         </div>
 </template>
